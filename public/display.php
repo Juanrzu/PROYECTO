b611a35e7fdda381381c202e86beba7d85d53b35
Juanrzu <juanrzu.16@gmail.com>
2025-04-07 00:24:42 -0400

    si

--- public/display.php
+++ public/display.php
@@ -18,7 +18,7 @@ include 'contador_sesion.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inicio</title>
+  <title>Prueba</title>
   <link rel="stylesheet" href="http://localhost/dashboard/Proyecto/src/css/styles.css">
 </head>
 
