084be30da389d6ce9bb13ac9eaf155aae00c10d2
Robertx3413 <robertomorado.coro2@gmail.comConnect with me>
2025-04-10 05:02:17 -0400

    cambios

--- public/constancias/aceptacion/crear.php
+++ public/constancias/aceptacion/crear.php
@@ -14,6 +14,7 @@ if (!isset($usuario)) {
 <html lang="en">
 
 <head>
+  
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="http://localhost/dashboard/Proyecto/src/css/styles.css">
