605b8ef7b6d571fe945df72a07933360771c69a8
Juanrzu <juanrzu.16@gmail.com>
2025-04-10 02:13:24 -0400

    header

--- public/header.php
+++ public/header.php
@@ -58,6 +58,26 @@
           <span class="flex-1 ms-3 whitespace-nowrap">Crear Constancia</span>
         </a>
       </li>
+        <!-- Trabajadores -->
+        <li>
+          <a href="http://localhost/dashboard/Proyecto/public/trabajadores/trabajadores.php" class="flex items-center p-2 text-gray-700 rounded-lg hover:bg-blue-50 group transition-colors">
+            <svg class="w-5 h-5 text-gray-500 group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+              <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+            </svg>
+            <span class="ml-3">Trabajadores</span>
+          </a>
+        </li>
+
+        <!-- Estudiantes Retirados -->
+        <li>
+          <a href="http://localhost/dashboard/Proyecto/public/estudiantes_retirados.php" class="flex items-center p-2 text-gray-700 rounded-lg hover:bg-blue-50 group transition-colors">
+            <svg class="w-5 h-5 text-gray-500 group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+              <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+            </svg>
+            <span class="ml-3">Estudiantes Retirados</span>
+          </a>
+        </li>
+      </ul>
       </ul>
 
       <div class="container font-bold mt-[100%]">
