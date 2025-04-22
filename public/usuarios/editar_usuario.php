3a45a546d74c116e447ed98419b0ca9097aab80d
Juanrzu <juanrzu.16@gmail.com>
2025-04-21 20:13:25 -0400

    modalesss

--- public/usuarios/editar_usuario.php
+++ public/usuarios/editar_usuario.php
@@ -155,7 +155,7 @@ $intentos_fallidos = $row['intentos_fallidos'];
                                 </button>
                             </div>
                             <div class="p-4 space-y-4">
-                                <p class="text-sm text-gray-600">¿Está seguro que quiere editar este trabajador?</p>
+                                <p class="text-sm text-gray-600">¿Está seguro que quiere editar este usuario?</p>
                             </div>
                             <div class="flex items-center p-4 border-t border-gray-300">
                                 <button data-modal-hide="default-modal" type="submit" name="submit" id="btn" class="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 shadow-sm">
